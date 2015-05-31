7ab45164a998c5bba4a886072eb7ef512a3a6467
Jan Sorensen <jas@praqma.net>
2015-05-31 21:02:32 +0200

    Make OS_PROJECT_NAME configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,7 @@ checkEnvDefined ZETTA_PASSWORD
 
 export OS_AUTH_URL=https://identity.api.zetta.io/v3
 export OS_REGION_NAME="no-osl1"
-export OS_PROJECT_ID=bceffc437a324a64ac708205dcad8208
-export OS_PROJECT_NAME="Standard"
+export OS_PROJECT_NAME=${ZETTA_PROJECT_NAME:-Standard}
 export OS_IDENTITY_API_VERSION="3"
 
 export OS_PROJECT_DOMAIN_ID=$ZETTA_DOMAIN_ID
