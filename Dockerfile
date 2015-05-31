89c1655245c823ad01b91f3092d572ec6f359d2c
Jan Sorensen <jas@praqma.net>
2015-05-31 20:19:50 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:14.04
+
+MAINTAINER Praqma <info@praqma.net>
+
+RUN apt-get update && apt-get install -y python-dev python-setuptools libffi-dev \
+            libssl-dev ca-certificates openssh-client && apt-get clean \
+    && easy_install pip \
+    && pip install python-openstackclient python-novaclient \
+           python-swiftclient python-heatclient python-cinderclient python-keystoneclient \
+           python-neutronclient python-designateclient docker-compose
+
+ENV PATH=/root/bin:$PATH
+
+ADD entrypoint.sh /entrypoint.sh
+ADD bin /root/bin
+
+VOLUME /root/.docker/machine
+VOLUME /config
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# /config is supposed to be mounted, and contain custom init files
+source /config/*.sh
+
+checkEnvDefined() {
+    [ $(eval echo \$$1) ] || (echo "ERROR: $1 not defined" ; exit 1)
+}
+
+# The init files in /config must define the following env vars
+checkEnvDefined ZETTA_DOMAIN_NAME
+checkEnvDefined ZETTA_DOMAIN_ID
+checkEnvDefined ZETTA_USERNAME
+checkEnvDefined ZETTA_PASSWORD
+
+export OS_AUTH_URL=https://identity.api.zetta.io/v3
+export OS_REGION_NAME="no-osl1"
+export OS_PROJECT_ID=bceffc437a324a64ac708205dcad8208
+export OS_PROJECT_NAME="Standard"
+export OS_IDENTITY_API_VERSION="3"
+
+export OS_PROJECT_DOMAIN_ID=$ZETTA_DOMAIN_ID
+export OS_USER_DOMAIN_ID=$ZETTA_DOMAIN_ID
+
+export OS_USERNAME=$ZETTA_USERNAME
+export OS_PASSWORD=$ZETTA_PASSWORD
+
+export OS_TENANT_NAME=$OS_PROJECT_NAME
+
+export ZETTA_FLAVOR_ID=${ZETTA_FLAVOR_ID:-6}
+
+exec "$@"
